37cb8854cf588914c4c2d0f27f4b7b04646156a9
Nadeem Ahmad <nadeemahmad1119990@gmail.com>
2023-08-03 15:08:40 +0530

    initial commmit

---
+++ data/database.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDb = () => {
+  mongoose
+   .connect(process.env.MONGO_URL,{
+     dbName: "backendapi",
+  })
+   .then(()=> console.log("Database connected"))
+   .catch(e=> console.log(e)); 
+};
+ 
